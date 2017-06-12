79e48e3d8d3f0a3dff1fb21c3bd24c2bd796cbb3
ciecimac <ciecclm@imnu.edu.cn>
2017-06-12 10:31:15 +0800

    又加了一行
    日期

--- testc/main.c
+++ testc/main.c
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     printf("2017年5月31日 09:41\n");
+    printf("2017年5月31日 09:41\n");
     return 0;
 }
